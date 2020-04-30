eb46ed4d0249352e6a0709eecf2e08728c836b9c
Taewoo kim <intelpro@kaist.ac.kr>
2020-04-30 19:29:34 +0900

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 
 
-python train_net.py --config-file configs/R_50_1x.yaml --num-gpus 1 SOLVER.IMS_PER_BATCH 4
+python train_net.py --config-file configs/R_50_1x.yaml --num-gpus 4 SOLVER.IMS_PER_BATCH 16
